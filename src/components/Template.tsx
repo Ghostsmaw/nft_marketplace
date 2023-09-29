32e880973ac72dcc9d28e7a38d15e554752c093b
Opeyemi Mansoor <opeyemiademon@gmail.com>
2023-09-29 10:18:37 +0100

    Design Completed

---
+++ src/components/Template.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './header'
+import Footer from './footer'
+
+
+const Template =(props:{children?:React.ReactNode, name?:string})=> {
+  return (<>
+  
+  <Header name={props.name} />
+
+
+{props.children}
+
+  <Footer />
+  </>
+  )
+}
+
+export default Template
---
+++ src/components/include.tsx
@@ -0,0 +1,9 @@
+
+export const Token = process.env.REACT_APP_TOKEN;
+
+
+export const config = {
+    headers: { 
+        'content-type': 'application/json',
+    'X-API-KEY': `${Token}`}
+} 
